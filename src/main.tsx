8802c0d1195885bf37c378f772156a42ab277251
Samuel Alves <samuel.estudo778@gmail.com>
2022-09-28 11:46:44 -0300

    chore: add chakra ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
 
 // styles
@@ -8,6 +8,8 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
 );
