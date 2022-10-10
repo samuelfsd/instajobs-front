433e29ca87f623a71ec368fa18057643ef20d1a6
Marcio <marciofilho@pecege.com>
2022-10-10 17:26:56 -0300

    feat: creating post component

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,24 +1,36 @@
-import { Box, Button, Flex, Input, useTheme, Spacer, Center, FormControl,
-  FormLabel,FormHelperText} from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Flex,
+  Input,
+  useTheme,
+  Spacer,
+  Center,
+  FormControl,
+  FormLabel,
+  FormHelperText,
+} from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
-import './styles.scss'
-import { IoReturnUpBack } from 'react-icons/io5'
-import { AiOutlineGoogle } from 'react-icons/ai'
+import './styles.scss';
+import { IoReturnUpBack } from 'react-icons/io5';
+import { AiOutlineGoogle } from 'react-icons/ai';
 
 export default function LoginPage() {
-  const theme = useTheme()
+  const theme = useTheme();
   return (
     <>
       <Box bgColor={theme.colors.primary} px={4} py={4}>
         <Flex alignItems={'center'}>
-          <div >
-            <Link to="/"><IoReturnUpBack size={35} color={'#B1B2FF'}/></Link>
+          <div>
+            <Link to='/'>
+              <IoReturnUpBack size={35} color={'#B1B2FF'} />
+            </Link>
           </div>
           <Spacer />
           <Flex alignItems={'center'}>
             <div>
-              <Link to="/register">
-                <Button px={2} h={10} borderRadius={'2xl'}  bgColor='#B1B2FF' variant='solid'>
+              <Link to='/register'>
+                <Button px={2} h={10} borderRadius={'2xl'} bgColor='#B1B2FF' variant='solid'>
                   Registrar
                 </Button>
               </Link>
@@ -27,8 +39,16 @@ export default function LoginPage() {
         </Flex>
       </Box>
       <Center>
-        <Box pt={10} boxShadow='dark-lg' w={'510px'} h={'540px'} p='6' rounded='md' bgColor={'#D2DAFF'}>
-          <Center textAlign={'center'} >
+        <Box
+          pt={10}
+          boxShadow='dark-lg'
+          w={'510px'}
+          h={'540px'}
+          p='6'
+          rounded='md'
+          bgColor={'#D2DAFF'}
+        >
+          <Center textAlign={'center'}>
             <h1>Entrar</h1>
           </Center>
           <Center>
@@ -42,20 +62,24 @@ export default function LoginPage() {
             <Input type='password' />
             <FormHelperText>Não esqueça de colocar sua senha</FormHelperText>
           </FormControl>
-          <Center h='50px' color='white' >
-            <Button px={6} borderRadius={'2xl'}  bgColor='#17E1B1' variant='solid'>
+          <Center h='50px' color='white'>
+            <Button px={6} borderRadius={'2xl'} bgColor='#17E1B1' variant='solid'>
               Login
             </Button>
           </Center>
-          <Center h='50px' color='white' >
-            <Button leftIcon={<AiOutlineGoogle />} px={6} borderRadius={'2xl'}  bgColor='#17E1B1' variant='solid'>
+          <Center h='50px' color='white'>
+            <Button
+              leftIcon={<AiOutlineGoogle />}
+              px={6}
+              borderRadius={'2xl'}
+              bgColor='#17E1B1'
+              variant='solid'
+            >
               Entrar com o Google
             </Button>
           </Center>
         </Box>
       </Center>
-      
-      
     </>
   );
 }
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes as AllRoutes } from 'react-router-dom';
 import HomePage from '~/pages/Home';
 import LoginPage from '~/pages/Login';
+import PostsPage from '~/pages/Posts';
 
 export function Routes() {
   return (
     <AllRoutes>
       <Route path='/login' element={<LoginPage />} />
       <Route path='/' element={<HomePage />} />
+      <Route path='/posts' element={<PostsPage />} />
     </AllRoutes>
   );
 }
