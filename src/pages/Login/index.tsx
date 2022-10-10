2e3bf5cd90d402a3d69b4c2b555c3fb52a2b4396
Samuel Alves <samuel.estudo778@gmail.com>
2022-10-10 16:36:40 -0300

    fix: change layout of loginPage

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -5,58 +5,64 @@ import {
   Button,
   Stack,
   Box,
-  Link,
   FormControl,
   useColorModeValue,
   Text,
   FormLabel,
+  Image,
 } from '@chakra-ui/react';
+import { ToggleColorMode } from '~/components/ToggleColorMode';
 
-import { Header } from '~/components/Header';
+import logo from '../../assets/logo.svg';
 
 export function LoginPage() {
   return (
-    <>
-      <Header />
-      <Flex
-        minH={'100vh'}
-        align={'center'}
-        justify={'center'}
-        bg={useColorModeValue('gray.50', 'gray.800')}
-      >
-        <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-          <Stack align={'center'}>
-            <Heading fontSize={'4xl'}>Entre com sua conta!</Heading>
-            <Text fontSize={'lg'} color={'gray.600'}>
-              Poste ofertas ou fique por dentro das novidades de emprego no mercado!
-            </Text>
-          </Stack>
-          <Box rounded={'lg'} bg={useColorModeValue('white', 'gray.700')} boxShadow={'lg'} p={8}>
-            <Stack spacing={4}>
-              <FormControl id='email'>
-                <FormLabel>Email </FormLabel>
-                <Input type='email' />
-              </FormControl>
-              <FormControl id='password'>
-                <FormLabel>Senha </FormLabel>
-                <Input type='password' />
-              </FormControl>
-              <Stack spacing={10}>
-                <Stack direction={{ base: 'column', sm: 'row' }} align={'flex-end'}></Stack>
-                <Button
-                  bg={'#17E1B1'}
-                  color={'white'}
-                  _hover={{
-                    bg: '#118268',
-                  }}
-                >
-                  Entrar
-                </Button>
-              </Stack>
-            </Stack>
+    <Flex
+      minH={'100vh'}
+      align={'center'}
+      justify={'center'}
+      bg={useColorModeValue('gray.50', 'gray.800')}
+    >
+      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+        <Stack align={'center'}>
+          <Box>
+            <Image boxSize='100px' src={logo} alt='Logo InstaJobs' />
           </Box>
+          <Heading fontSize={'4xl'}>Entre com sua conta!</Heading>
+          <Text align='center' fontSize={'lg'} color={useColorModeValue('gray.500', 'gray.200')}>
+            Poste ofertas ou fique por dentro das novidades de emprego no mercado!
+          </Text>
         </Stack>
-      </Flex>
-    </>
+        <Box rounded={'lg'} bg={useColorModeValue('white', 'gray.700')} boxShadow={'lg'} p={8}>
+          <Stack align='flex-end' spacing={5}>
+            <Box>
+              <ToggleColorMode />
+            </Box>
+          </Stack>
+          <Stack spacing={4}>
+            <FormControl id='email'>
+              <FormLabel>Email </FormLabel>
+              <Input type='email' />
+            </FormControl>
+            <FormControl id='password'>
+              <FormLabel>Senha </FormLabel>
+              <Input type='password' />
+            </FormControl>
+            <Stack spacing={10}>
+              <Stack direction={{ base: 'column', sm: 'row' }} align={'flex-end'}></Stack>
+              <Button
+                bg={'#17E1B1'}
+                color={'white'}
+                _hover={{
+                  bg: '#118268',
+                }}
+              >
+                Entrar
+              </Button>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
   );
 }
