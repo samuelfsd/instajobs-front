3d3f467d9b2c96de63d13127a5470557227e2886
Marcio <marciofilho@pecege.com>
2022-10-14 15:52:04 -0300

    refactor: insta jobs route layout

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,14 +1,24 @@
-import { Route, Routes as AllRoutes } from 'react-router-dom';
-import HomePage from '~/pages/Home';
+import { Outlet, Route, Routes as AllRoutes } from 'react-router-dom';
+import { Header } from '~/components/Header';
+import { HomePage } from '~/pages/Home';
 import { LoginPage } from '~/pages/Login';
-import PostsPage from '~/pages/Posts';
+
+function InstaJobsLayout() {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+    </div>
+  );
+}
 
 export function Routes() {
   return (
     <AllRoutes>
-      <Route path='/login' element={<LoginPage />} />
-      <Route path='/' element={<HomePage />} />
-      <Route path='/posts' element={<PostsPage />} />
+      <Route path='/' element={<LoginPage />} />
+      <Route element={<InstaJobsLayout />}>
+        <Route path='/home' element={<HomePage />} />
+      </Route>
     </AllRoutes>
   );
 }
