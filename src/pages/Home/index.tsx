c90f8fa6a7e06247200c9b3f5f4e23c26768fd0f
Marcio <marciofilho@pecege.com>
2022-10-14 15:31:13 -0300

    style: fix layout header

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,28 +1,12 @@
-import { Box, Flex, Spacer, useTheme } from '@chakra-ui/react';
-import { HiOutlineUserCircle } from 'react-icons/hi';
-import { Link } from 'react-router-dom';
+import { Box, useTheme } from '@chakra-ui/react';
+import { Header } from '~/components/Header';
 
 export default function HomePage() {
   const theme = useTheme();
   return (
     <>
-      <Box bgColor={theme.colors.primary} px={4} py={4}>
-        <Flex alignItems={'center'}>
-          <div>logo</div>
-          <Spacer />
-          <Flex alignItems={'center'}>
-            <div>
-              <Link to='/login'>
-                <p>Login</p>
-              </Link>
-            </div>
-            <div className='icon'>
-              <Link to='/login'>
-                <HiOutlineUserCircle />
-              </Link>
-            </div>
-          </Flex>
-        </Flex>
+      <Box bgColor={theme.colors.primary}>
+        <Header />
       </Box>
       <h1 className='teste'>Bem vindo a página principal</h1>
     </>
